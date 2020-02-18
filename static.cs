6f6bd13db23a4c18ddee763f97dad5987d144bfe
Manraj Pannu <toxekgfx@gmail.com>
2020-02-18 00:21:14 -0500

    Add files via upload

---
+++ static.cs
@@ -0,0 +1,33 @@
+body {
+    height: 100%;
+    width: 100%;
+    margin:0;
+    padding: 0;
+}
+video {
+    height: auto;
+    min-height:100%;
+    width: 100%;
+    min-width: 100%;
+}
+.video_holder {
+    height: 100%;
+    min-height:100%;
+    width: 100%;
+    min-width: 100%;
+}
+header, footer {
+    height: 50px;
+    width: 100%;
+    background: #0096ff;
+}
+
+.rights() {
+    position: absolute; 
+    right: 0;
+
+}
+
+.form {
+    display: inline-block; //Or display: inline; 
+}
